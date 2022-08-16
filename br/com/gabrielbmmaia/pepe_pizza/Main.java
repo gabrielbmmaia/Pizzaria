60ba2088fdf5a232884d168cfa32a30f52cce448
gabrielbmmaia <gbmmaia.contato@gmail.com>
2022-08-16 00:48:28 -0300

    Implementado o metodo fazerPedido da classe Pedido com seleção da pizza, tamanho da pizza, escolha de borda recheada

--- br/com/gabrielbmmaia/pepe_pizza/Main.java
+++ br/com/gabrielbmmaia/pepe_pizza/Main.java
@@ -1,6 +1,5 @@
 package br.com.gabrielbmmaia.pepe_pizza;
 
-import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
@@ -8,24 +7,7 @@ public class Main {
     public static int resposta;
         public static void main(String[] args) {
 
-            System.out.println("--- Bem-Vindo a Pizzaria Pepe! ---");
-            Scanner sc = new Scanner(System.in);
-            Pizza pizza = new Pizza();
-            System.out.println("-> Este é nosso cardápio! Deseja qual sabor? <-");
-            System.out.println("1 - Pepperoni");
-            System.out.println("2 - Quatro Queijos");
-            System.out.println("3 - Calabresa");
-            System.out.println("4 - Frango e Catupiry");
-            System.out.println("5 - Marguerita");
-            pizza.setSabor(sc.nextInt());
-            System.out.println("! Ótima Escolha !");
-            System.out.println("-> Agora, qual seria o tamanho da pizza? <-");
-            System.out.println("1 - Pequena");
-            System.out.println("2 - Média");
-            System.out.println("3 - Grande");
-            System.out.println("4 - Família");
-
-
-
+            Pedido pedido = new Pedido();
+            pedido.fazerPedido();
     }
 }
--- br/com/gabrielbmmaia/pepe_pizza/Pedido.java
+++ br/com/gabrielbmmaia/pepe_pizza/Pedido.java
@@ -1,4 +1,35 @@
 package br.com.gabrielbmmaia.pepe_pizza;
 
+import java.util.Scanner;
+
 public class Pedido {
+
+    private int resposta = 1;
+    Pizza pizza = new Pizza();
+    Scanner sc = new Scanner(System.in);
+
+    public void fazerPedido() {
+        System.out.println("------------------------------");
+        System.out.println("-> Este é nosso cardápio! Deseja qual sabor? <-\n");
+        System.out.println("1 - Pepperoni \n(Molho de Tomate - Mussarela - Pepperoni)\n");
+        System.out.println("2 - Quatro Queijos \n(Molho de Tomate - Mussarela - Parmesão - Provolone - Gorgonzola)\n");
+        System.out.println("3 - Calabresa \n(Molho de Tomate - Mussarela - Calabresa - Cebola)\n");
+        System.out.println("4 - Frango e Catupiry \n(Molho de Tomate - Mussarela - Frango Desfiado - Catupiry - Cebola)\n");
+        System.out.println("5 - Marguerita \n(Molho de Tomate - Mussarela - Tomate - Mangericão)\n");
+        pizza.setSabor(sc.nextInt());
+        System.out.println("------------------------------");
+        System.out.println("-> Agora, qual seria o tamanho da pizza? <-");
+        System.out.println("1 - Pequena");
+        System.out.println("2 - Média");
+        System.out.println("3 - Grande");
+        System.out.println("4 - Família");
+        pizza.setTamanho(sc.nextInt());
+        System.out.println("------------------------------");
+        System.out.println("-> Gostaria de Borda Recheda ? <-");
+        System.out.println("1 - Sem Borda Recheada");
+        System.out.println("2 - Com Borda Recheada");
+        pizza.setBordaRecheada(sc.nextInt());
+        System.out.println("------------------------------");
+
+    }
 }
